a9b5c608507f57bbd1f57dd99f1a4622e1f99921
hardgard <akohardgard@gmailcom>
2021-06-21 21:37:15 +0300

    2.1 dbs populates

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM meals;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +13,9 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, id, date_time, description, calories_per_day)
+VALUES (100001, 100006, TIMESTAMP '2021-10-19 11:23:54', 'Lunch', 1500),
+       (100000, 100002, TIMESTAMP '2021-10-19 11:23:54', 'Braeakfast', 500),
+       (100001, 100003, TIMESTAMP '2021-10-21 10:23:54', 'Dinner', 1500),
+       (100001, 100004, TIMESTAMP '2021-10-22 10:23:54', 'Dinner', 1500);
