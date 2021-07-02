e13039205951e2607ceca3d0ff23a2622be521b1
hardgard <akohardgard@gmailcom>
2021-07-02 21:13:22 +0300

    Revert "10 of 13 tests passed"
    This reverts commit 0b2fd1d6

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,38 +1,17 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-
-@NamedQueries({
-        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
-        @NamedQuery(name = Meal.ALL, query = "SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY  m.dateTime desc "),
-        @NamedQuery(name = Meal.GET_BETWEEN_HALF_OPEN, query = "SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime >= :startDateTime AND m.dateTime < :endDateTime ORDER BY m.dateTime desc "),
-})
-@Entity
-@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_Id", "date_time"}, name = "meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
-
-    public static final String DELETE = "Meal.delete";
-    public static final String ALL = "Meal.getAll";
-    public static final String GET_BETWEEN_HALF_OPEN = "Meal.getBetweenHalfOpen";
-
-    @Column(name = "date_time", nullable = false)
-    @NotNull
     private LocalDateTime dateTime;
 
-    @Column(name = "description", nullable = false)
-    @NotBlank
     private String description;
 
-    @Column(name = "calories", nullable = false)
-    @NotNull
     private int calories;
-
 
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
